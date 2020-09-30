4e6cd2a0a8b801bea8a4369cd8a5e4be8af166ce
Jaime <52935916+jaimey@users.noreply.github.com>
2020-09-30 14:24:57 -0500

    Change to PHP 7.2

--- phpSwitcher.sh
+++ phpSwitcher.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 PHP_VERSION=$1
-if [ "$PHP_VERSION" == "php7.1" ] || [ "$PHP_VERSION" == "7" ]; then
-    sudo update-alternatives --set php /usr/bin/php7.1
-    sudo update-alternatives --set phar /usr/bin/phar7.1
-    sudo update-alternatives --set phar.phar /usr/bin/phar.phar7.1
+if [ "$PHP_VERSION" == "php7.2" ] || [ "$PHP_VERSION" == "7" ]; then
+    sudo update-alternatives --set php /usr/bin/php7.2
+    sudo update-alternatives --set phar /usr/bin/phar7.2
+    sudo update-alternatives --set phar.phar /usr/bin/phar.phar7.2
 
     sudo a2dismod php5.6
-    sudo a2enmod php7.1
+    sudo a2enmod php7.2
 
     sudo service apache2 restart
     sudo service mysql restart
@@ -17,7 +17,7 @@ elif [ "$PHP_VERSION" == "php5.6" ] || [ "$PHP_VERSION" == "5" ]; then
     sudo update-alternatives --set phar /usr/bin/phar5.6
     sudo update-alternatives --set phar.phar /usr/bin/phar.phar5.6
 
-    sudo a2dismod php7.1
+    sudo a2dismod php7.2
     sudo a2enmod php5.6
 
     sudo service apache2 restart
@@ -25,5 +25,5 @@ elif [ "$PHP_VERSION" == "php5.6" ] || [ "$PHP_VERSION" == "5" ]; then
     sudo service memcached restart
     sudo service ssh restart
 else
-    echo "usage ./switch-php.sh [php5.6, php7.1, 5, 7]"
+    echo "usage ./switch-php.sh [php5.6, php7.2, 5, 7]"
 fi
