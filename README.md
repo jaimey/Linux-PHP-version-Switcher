224eae8617eef84ced836b9ca8e9e21d3612fd9a
Jaime Yepes <jayepesviloria@gmail.com>
2022-12-22 19:36:33 -0500

    feat: add php8.1

--- README.md
+++ README.md
@@ -3,14 +3,9 @@ Script to easy switch between php versions in Linux or WSL
 
 ### Usage
 ```
-./switch-php.sh [php5.6, 5, php7.2, 72, 73, 74]
+./switch-php.sh [74, 81]
 ```
 #### Example switch to php7.4
-```
-./switch-php.sh php7.4
-```
-
-or
 ```
 ./switch-php.sh 74
 ```
--- switch-php.sh
+++ switch-php.sh
@@ -1,49 +1,23 @@
 #!/bin/bash
 PHP_VERSION=$1
-if [ "$PHP_VERSION" == "php5.6" ] || [ "$PHP_VERSION" == "5" ]; then
-    sudo update-alternatives --set php /usr/bin/php5.6
-    sudo update-alternatives --set phar /usr/bin/phar5.6
-    sudo update-alternatives --set phar.phar /usr/bin/phar.phar5.6
+if [ "$PHP_VERSION" == "81" ]; then
+    sudo update-alternatives --set php /usr/bin/php8.1
+    sudo update-alternatives --set phar /usr/bin/phar8.1
+    sudo update-alternatives --set phar.phar /usr/bin/phar.phar8.1
 
-    sudo a2dismod php7.2
-    sudo a2dismod php7.3
     sudo a2dismod php7.4
-    sudo a2enmod php5.6
+    sudo a2enmod php8.1
 
     sudo service apache2 restart
-elif [ "$PHP_VERSION" == "php7.2" ] || [ "$PHP_VERSION" == "72" ]; then
-    sudo update-alternatives --set php /usr/bin/php7.2
-    sudo update-alternatives --set phar /usr/bin/phar7.2
-    sudo update-alternatives --set phar.phar /usr/bin/phar.phar7.2
-
-    sudo a2dismod php5.6
-    sudo a2dismod php7.3
-    sudo a2dismod php7.4
-    sudo a2enmod php7.2
-
-    sudo service apache2 restart
-elif [ "$PHP_VERSION" == "php7.3" ] || [ "$PHP_VERSION" == "73" ]; then
-    sudo update-alternatives --set php /usr/bin/php7.3
-    sudo update-alternatives --set phar /usr/bin/phar7.3
-    sudo update-alternatives --set phar.phar /usr/bin/phar.phar7.3
-
-    sudo a2dismod php5.6
-    sudo a2dismod php7.2
-    sudo a2dismod php7.4
-    sudo a2enmod php7.3
-
-    sudo service apache2 restart
-elif [ "$PHP_VERSION" == "php7.4" ] || [ "$PHP_VERSION" == "74" ]; then
+elif [ "$PHP_VERSION" == "74" ]; then
     sudo update-alternatives --set php /usr/bin/php7.4
     sudo update-alternatives --set phar /usr/bin/phar7.4
     sudo update-alternatives --set phar.phar /usr/bin/phar.phar7.4
 
-    sudo a2dismod php5.6
-    sudo a2dismod php7.2
-    sudo a2dismod php7.3
+    sudo a2dismod php8.1
     sudo a2enmod php7.4
 
     sudo service apache2 restart    
 else
-    echo "usage ./switch-php.sh [php5.6, 5, php7.2, 72, 73, 74]"
+    echo "usage ./switch-php.sh [74, 81]"
 fi
